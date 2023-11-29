92eb6251709ca149ea7581f5cabe4682a72d8d1f
quantulr <35954003+quantulr@users.noreply.github.com>
2023-11-30 00:18:20 +0800

    update

--- src/response/files.rs
+++ src/response/files.rs
@@ -14,8 +14,11 @@ pub enum EntryType {
 pub struct EntryItem {
     pub name: String,
     pub entry_type: EntryType,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub size: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub width: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub height: Option<u32>,
 }
 
